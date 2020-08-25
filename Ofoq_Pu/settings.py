bddb6bbd2099b1b41cc86cb9a03e0a2335ad2431
parsa1601 <parsaetemad1601@gmail.com>
2020-08-25 08:32:49 +0330

    implemented archive models.py and changing setting for being able to browse and add file from user computer

--- Ofoq_Pu/settings.py
+++ Ofoq_Pu/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'archive.apps.ArchiveConfig',
     'articles.apps.ArticlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = 'media'
+
