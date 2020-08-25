1896ca160a3dfb51f91a291efedcecb30da5c1cd
parsa1601 <parsaetemad1601@gmail.com>
2020-08-25 06:41:11 +0330

    implemented index in articles app

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
 urlpatterns = [
-    path('', views.articles_list, name='articles_list'),
+    path('', views.IndexView.as_view() , name='index'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,15 @@
 
 from django.http import HttpResponse
+from django.views import generic
+from .models import Article
 
+
+class IndexView(generic.ListView):
+    template_name = "articles\index.html"
+    context_object_name = "text"
+
+    def get_queryset(self):
+        return Article.objects.all()
 
 def articles_list(request):
     return HttpResponse("Temporary layout for the articles menu")
