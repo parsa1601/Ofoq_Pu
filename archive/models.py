32321a2ea1319168143dcdc76b39bac44995eaa0
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-08-25 14:31:17 +0430

    partly implemented articles/index.html

---
+++ archive/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class file(models.Model):
+    title = models.CharField(max_length=100)
+    type = models.CharField(max_length=10)
+    file_upl = models.FileField()
