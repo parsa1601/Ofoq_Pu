1a852f7cc1da8b56af73d80e4b9258a65dce245f
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-08-27 12:21:09 +0430

    Completed class Article in articles/models.py

--- articles/models.py
+++ articles/models.py
@@ -6,11 +6,47 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     summary = models.CharField(max_length=300)
     pub_date = models.DateField('تاریخ انتشار:')
+    main_image = models.ImageField(
+        upload_to='static/articles/images',
+        blank=True
+    )
     content1 = models.CharField(max_length=4000)
-    content2 = models.CharField(max_length=4000)
-    content3 = models.CharField(max_length=4000)
-    content4 = models.CharField(max_length=4000)
-    content5 = models.CharField(max_length=4000)
+    image1 = models.ImageField(
+        upload_to='static/articles/images',
+        blank=True
+    )
+    content2 = models.CharField(
+        max_length=4000,
+        blank=True
+    )
+    image2 = models.ImageField(
+        upload_to='static/articles/images',
+        blank=True
+    )
+    content3 = models.CharField(
+        max_length=4000,
+        blank=True
+    )
+    image3 = models.ImageField(
+        upload_to='static/articles/images',
+        blank=True
+    )
+    content4 = models.CharField(
+        max_length=4000,
+        blank=True
+    )
+    image4 = models.ImageField(
+        upload_to='static/articles/images',
+        blank=True
+    )
+    content5 = models.CharField(
+        max_length=4000,
+        blank=True
+    )
+    image5 = models.ImageField(
+        upload_to='static/articles/images',
+        blank=True
+    )
 
     def __str__(self):
         return self.title
