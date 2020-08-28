0156afa087433f7cbace4303625710532f75a7c1
parsa1601 <parsaetemad1601@gmail.com>
2020-08-28 17:00:57 +0330

    finish ofoqSchool without style

--- archive/models.py
+++ archive/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class PDF(models.Model):
-    Title = models.CharField(max_length=800)
-    Author = models.CharField(max_length=100)
-    Date = models.DateField(models.DateField('تاریخ انتشار:'))
-    file_url = models.FileField(upload_to='archive/pdfs/')
+    Title = models.CharField(
+                            'عنوان',
+                            max_length=800
+                            )
+    Author = models.CharField(
+                            'نویسنده',
+                            max_length=100
+                            )
+    Date = models.DateField(
+                            'تاریخ انتشار:'
+                            )
+    file_url = models.FileField(
+                                'آپلود فایل',
+                                upload_to='archive/audios/'
+                                )
 
     def __str__(self):
         return self.Title
